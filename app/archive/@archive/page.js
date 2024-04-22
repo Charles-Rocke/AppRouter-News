0caa054362bafddb33ef1daf4c006d40b6e8ef1a
Spook <spook.devv@gmail.com>
2024-04-22 18:40:15 -0400

    Setting up and working with parallel routes

---
+++ app/archive/@archive/page.js
@@ -0,0 +1,9 @@
+function ArchivePage() {
+  return (
+    <div>
+      <h1>Archive page</h1>
+    </div>
+  );
+}
+
+export default ArchivePage;
---
+++ app/archive/@latest/page.js
@@ -0,0 +1,9 @@
+function LatestNewsPage() {
+  return (
+    <div>
+      <h1>Latest News page</h1>
+    </div>
+  );
+}
+
+export default LatestNewsPage;
