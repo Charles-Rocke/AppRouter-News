23684874e2a1aef6eee5c4219f78a3b12070428f
Spook <spook.devv@gmail.com>
2024-04-22 18:06:16 -0400

    added app styling

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,22 @@
+import { DUMMY_NEWS } from "@/dummy-news";
 import Link from "next/link";
 
 function NewsPage() {
   return (
     <div>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="/news/first-news">First News item</Link>
-        </li>
-        <li>
-          <Link href="/news/second-news">Second News item</Link>
-        </li>
-        <li>
-          <Link href="/news/third-news">Third News item</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
