4007fcbc33ebd47757395a8e1660ebfbca4edaa6
Spook <spook.devv@gmail.com>
2024-04-22 18:09:01 -0400

    Handling not found errors and showing not found pages

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,10 @@
+function NewsNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Page not found</h1>
+      <p>Could not find requested article</p>
+    </div>
+  );
+}
+
+export default NewsNotFoundPage;
